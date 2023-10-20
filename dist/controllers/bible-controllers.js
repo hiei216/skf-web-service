1ee1427f9bcce10abd9a0af2d832ce4d9f457c1d
Jiri Dvorak <jiri.dvorak@check24.de>
2023-10-20 12:54:30 +0200

    Add server test

--- dist/controllers/bible-controllers.js
+++ dist/controllers/bible-controllers.js
@@ -1,34 +1,28 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createVerses = void 0;
-const verse_1 = __importDefault(require("../models/verse"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const createVerses = async (req, res, next) => {
-    const createdVerse = new verse_1.default({
-        createdAt: new Date(),
-        participants: [
-            {
-                firstName: "Jirko",
-                lastName: "Dvorak",
-                email: "jiri.dvorak@gmx.de",
-                verse: "This is an cool bible verse!!",
-            },
-        ],
-    });
-    try {
-        const sess = await mongoose_1.default.startSession();
-        sess.startTransaction();
-        await createdVerse.save({ session: sess });
-        await sess.commitTransaction();
-    }
-    catch (err) {
-        console.log('err', err);
-        return next(err);
-    }
-    res.status(201).json({ verses: '??' });
+    //   const createdVerse = new Verse({
+    //     createdAt: new Date(),
+    //     participants: [
+    //       {
+    //         firstName: "Jirko",
+    //         lastName: "Dvorak",
+    //         email: "jiri.dvorak@gmx.de",
+    //         verse: "This is an cool bible verse!!",
+    //       },
+    //     ],
+    //   });
+    //   try {
+    //     const sess = await mongoose.startSession();
+    //     sess.startTransaction();
+    //     await createdVerse.save({ session: sess });
+    //     await sess.commitTransaction();
+    //   } catch (err) {
+    //     console.log('err', err)
+    //     return next(err);
+    //   }
+    res.status(201).json({ verses: 'no verses' });
 };
 exports.createVerses = createVerses;
 //# sourceMappingURL=bible-controllers.js.map
--- src/controllers/bible-controllers.ts
+++ src/controllers/bible-controllers.ts
@@ -3,28 +3,28 @@ import { RequestHandler } from "express";
 import mongoose from "mongoose";
 
 export const createVerses: RequestHandler = async (req, res, next) => {
-  const createdVerse = new Verse({
-    createdAt: new Date(),
-    participants: [
-      {
-        firstName: "Jirko",
-        lastName: "Dvorak",
-        email: "jiri.dvorak@gmx.de",
-        verse: "This is an cool bible verse!!",
-      },
-    ],
-  });
+//   const createdVerse = new Verse({
+//     createdAt: new Date(),
+//     participants: [
+//       {
+//         firstName: "Jirko",
+//         lastName: "Dvorak",
+//         email: "jiri.dvorak@gmx.de",
+//         verse: "This is an cool bible verse!!",
+//       },
+//     ],
+//   });
 
 
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await createdVerse.save({ session: sess });
-    await sess.commitTransaction();
-  } catch (err) {
-    console.log('err', err)
-    return next(err);
-  }
+//   try {
+//     const sess = await mongoose.startSession();
+//     sess.startTransaction();
+//     await createdVerse.save({ session: sess });
+//     await sess.commitTransaction();
+//   } catch (err) {
+//     console.log('err', err)
+//     return next(err);
+//   }
 
-  res.status(201).json({ verses: '??' });
+  res.status(201).json({ verses: 'no verses' });
 };
