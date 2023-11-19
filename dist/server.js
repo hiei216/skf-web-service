11e774aeaf03fec0deeea861c432da56bcef5633
Jiri Dvorak <jiri.dvorak@gmx.de>
2023-11-19 23:22:28 +0100

    Fix cors

--- dist/server.js
+++ dist/server.js
@@ -6,11 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const bible_1 = __importDefault(require("./routes/bible"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8081;
 app.use(express_1.default.json()); // Add this line to enable JSON parsing in the request body
-app.use((0, cors_1.default)());
+// app.use(cors());
+app.use((err, req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use("/bible", bible_1.default);
 app.get("/", (req, res) => {
     res.send("Hello, TypeScript Express!");
